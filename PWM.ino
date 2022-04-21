85f4764a79009dc4190b1562d078e28d453a5d01
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-21 23:00:12 +0530

    Add files via upload

---
+++ PWM.ino
@@ -0,0 +1,21 @@
+int led = 9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(led,0);
+  delay(1000);
+  analogWrite(led,64);
+  delay(1000);
+  analogWrite(led,128);
+  delay(1000);
+  analogWrite(led,192);
+  delay(1000);
+  analogWrite(led,255);
+  delay(1000);
+
+}
