c332da7ee4f9ced1483f781d8498bf00e54ed4f4
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-21 23:02:01 +0530

    Add files via upload

---
+++ RGB.ino
@@ -0,0 +1,24 @@
+int led1 =3;
+int led2 =5;
+int led3 =6;
+void setup() {
+  pinMode(led1,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  delay(1000);
+   digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  delay(1000);
+    digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  delay(1000);
+}
+  
+
