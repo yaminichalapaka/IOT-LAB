55d2ed8f374aceffd6b4cd0cb2544ed97ee1bc01
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-21 23:00:55 +0530

    Add files via upload

---
+++ _2ledBlink.ino
@@ -0,0 +1,27 @@
+int led1 = 10;
+int led2 = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  
+  
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+    digitalWrite(led1,HIGH);
+   
+    digitalWrite(led2,LOW);
+    delay(300);
+  
+    digitalWrite(led2,HIGH);
+   
+    digitalWrite(led1,LOW);
+    delay(300);
+
+delay(10);
+}
