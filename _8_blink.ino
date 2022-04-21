9eaacb3e3dff4d92dc7037e279721648412fe30a
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-21 23:00:37 +0530

    Add files via upload

---
+++ _8_blink.ino
@@ -0,0 +1,16 @@
+int l[]=13,12,11,10,9,8,7,6;
+int i;
+void setup() {
+  // put your setup code here, to run once:
+  for(i=0;i<=7;i++)
+pinMode(l[i],OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(i=0;i<=7:i++){
+   digitalWrite(l[i],HIGH);
+   delay(100);
+   digitalWrite(l[i],LOW);
+   delay(100);
+}
