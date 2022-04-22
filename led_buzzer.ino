466d2fecb0e210f269f58963ec942160b9a3043b
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-22 17:58:16 +0530

    Add files via upload

---
+++ led_buzzer.ino
@@ -0,0 +1,20 @@
+int led=9;
+int btn=2;
+int temp;
+void setup() {
+  pinMode(led,OUTPUT);
+  pinMode(btn,INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  temp=digitalRead(btn);
+  if(temp==1)
+  digitalWrite(led,HIGH);
+  else
+  digitalWrite(led,LOW);
+  Serial.print("value of button "); 
+  Serial.println(temp);
+  
+
+}
