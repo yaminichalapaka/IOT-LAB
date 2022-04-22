e8396490fc82ea3328f783d6764f1bb6c9aa42cf
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-22 17:58:55 +0530

    Add files via upload

---
+++ DIGITAL_VOLTMETER.ino
@@ -0,0 +1,14 @@
+int sensorPin=A0;
+int sensorValue=0;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue=analogRead(sensorPin);
+  float voltage=sensorValue*(5.0/1023.0);
+  Serial.print(voltage);
+  Serial.println();
+  delay(1000);
+
+}
