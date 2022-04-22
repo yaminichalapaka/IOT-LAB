b3b8f75a5b29c76c9651c6589d832ee0675cbee8
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2022-04-22 17:57:34 +0530

    Add files via upload

---
+++ led_btn.ino
@@ -0,0 +1,14 @@
+int btn=2;
+int temp;
+void setup() {
+  pinMode(btn,INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  temp=digitalRead(btn);
+  Serial.print("value of button "); 
+  Serial.println(temp);
+  
+
+}
